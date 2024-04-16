5ec1e343aebc6c9043931099cb35b021b4182246
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 08:45:21 -0300

    Update and rename Questão 4.cpp to Questão 4.c

--- Questão 4.c
+++ Questão 4.c
@@ -4,13 +4,13 @@
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float q;
-	printf("Digite a quantidade de maçãs desejada: ");
+	printf("Digite a quantidade de maÃ§Ã£s desejada: ");
 	scanf("%f", &q);
 
 	if(q < 12){
-		printf("\nO valor a ser pago é: R$ %.2f", q * 1.30);
+		printf("\nO valor a ser pago Ã©: R$ %.2f", q * 1.30);
 	} else if (q >= 12){
-		printf("\nO valor a ser pago é: R$ %.2f", q * 1.10);
+		printf("\nO valor a ser pago Ã©: R$ %.2f", q * 1.10);
 	}
 	
 	return(0);

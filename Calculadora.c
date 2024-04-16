025e455445de043cbf470a0b7ff38462c01141d3
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 15:13:47 -0300

    Update and rename Questão 3.c to Calculadora.c

--- Calculadora.c
+++ Calculadora.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 #include<locale.h>
-
+/*
+Crie um programa que simule uma calculadora simples, ou seja, que efetua as
+quatro operações básicas da matemática (adição, subtração, multiplicação e divisão)
+para dois valores reais fornecidos pelo usuário. O programa deve exibir as opções
+ao usuário e efetuar uma delas, conforme a opção digitada pelo usuário. Para isso,
+utilize a estrutura SWITCH.
+Obs.: não esqueça que a divisão só deve ser realizada, caso o denominador seja
+diferente de zero. Caso contrário, o programa deve exibir uma mensagem
+informando que a divisão é indefinida.
+*/
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	float n1, n2;

1314b4f70c70217fb33d21b8b14ced156b47fd06
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 15:09:09 -0300

    Update Divisibilidade.c

--- Divisibilidade.c
+++ Divisibilidade.c
@@ -5,6 +5,10 @@ A determinação da divisibilidade de um número por outro é importante na
 programação. Sabendo disso, crie um programa que solicita e lê dois números
 inteiros (numerador e denominador) e verifica se a divisão é exata. Se for, exiba a
 mensagem: “É divisível!”. Caso contrário, exiba a mensagem: “Não é divisível!”.
+
+Obs.: não esqueça que a divisão só deve ser realizada, caso o denominador seja
+diferente de zero. Caso contrário, o programa deve exibir uma mensagem
+informando que a divisão é indefinida.
 */
 int main(){
 	setlocale(LC_ALL, "Portuguese");

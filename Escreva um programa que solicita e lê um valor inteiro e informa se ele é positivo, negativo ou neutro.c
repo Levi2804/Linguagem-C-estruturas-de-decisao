221d5326e97d639f6775b537667948faf4493cb9
Levi Araújo <166538311+Levi2804@users.noreply.github.com>
2024-04-16 15:05:20 -0300

    Update and rename Questão 1.c to Escreva um programa que solicita e lê um valor inteiro e informa se ele é positivo, negativo ou neutro.c

--- Escreva um programa que solicita e lê um valor inteiro e informa se ele é positivo, negativo ou neutro.c
+++ Escreva um programa que solicita e lê um valor inteiro e informa se ele é positivo, negativo ou neutro.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 #include<locale.h>
+/*
+Escreva um programa que solicita e lê um valor inteiro e informa se ele é positivo,
+negativo ou neutro.
+*/
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int n;
